18ad63f2866fd3dbfd9e1ab5e28a5c7aaf69bc33
devcui <devcui@outlook.com>
2023-10-12 23:03:18 +0800

    chore: format plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -31,10 +31,7 @@ M.config = function()
     -- A plugin to improve your rust experience in neovim.
     { "simrat39/rust-tools.nvim" },
     -- lsp_lines is a simple neovim plugin that renders diagnostics using virtual lines on top of the real line of code.
-    {
-      url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-      lazy = true,
-    },
+    { url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim", lazy = true },
     -- zen mod
     { "folke/zen-mode.nvim" },
     -- A search panel for neovim.
@@ -70,194 +67,68 @@ M.config = function()
     { "RishabhRD/nvim-cheat.sh" },
     -- Clipboard manager neovim plugin with telescope integration
     { "AckslD/nvim-neoclip.lua" },
-    {
-      "kristijanhusak/vim-dadbod-completion",
-    },
+    { "kristijanhusak/vim-dadbod-completion" },
     {
       "kristijanhusak/vim-dadbod-ui",
-      dependencies = {
-        {
-          "tpope/vim-dadbod",
-          lazy = true,
-        },
-      },
+      dependencies = { { "tpope/vim-dadbod", lazy = true } },
       lazy = true,
     },
-    {
-      "karb94/neoscroll.nvim",
-    },
-    {
-      "declancm/cinnamon.nvim",
-    },
-    {
-      "github/copilot.vim",
-    },
-    {
-      "zbirenbaum/copilot.lua",
-      dependencies = { "zbirenbaum/copilot-cmp", "nvim-cmp" },
-    },
-    {
-      "ThePrimeagen/harpoon",
-      dependencies = {
-        { "nvim-lua/plenary.nvim" },
-        { "nvim-lua/popup.nvim" },
-      },
-    },
-    {
-      "sindrets/diffview.nvim",
-    },
-    {
-      "chipsenkbeil/distant.nvim",
-    },
-    {
-      "abzcoding/nvim-mini-file-icons",
-    },
-    { "mtdl9/vim-log-highlighting", ft = { "text", "log" } },
-    {
-      "yamatsum/nvim-cursorline",
-    },
-    {
-      "abecodes/tabout.nvim",
-    },
-    {
-      "kevinhwang91/nvim-hlslens",
-    },
-    {
-      "chrisbra/csv.vim",
-    },
-    {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      lazy = true,
-      dependencies = "nvim-treesitter",
-    },
-    {
-      "sidebar-nvim/sidebar.nvim",
-    },
-    {
-      "skywind3000/asynctasks.vim",
-      dependencies = {
-        { "skywind3000/asyncrun.vim" },
-      },
-    },
-    {
-      "scalameta/nvim-metals",
-      dependencies = { "nvim-lua/plenary.nvim" },
-    },
-    {
-      "jbyuki/instant.nvim",
-    },
-    {
-      "nvim-telescope/telescope-file-browser.nvim",
-    },
-    {
-      "j-hui/fidget.nvim",
-    },
-    {
-      "michaelb/sniprun",
-    },
-    {
-      "liuchengxu/vista.vim",
-    },
-    {
-      "p00f/clangd_extensions.nvim",
-    },
-    {
-      "saecki/crates.nvim",
-      dependencies = { { "nvim-lua/plenary.nvim" } },
-    },
-    {
-      "hrsh7th/cmp-cmdline",
-    },
-    {
-      "mrjones2014/legendary.nvim",
-    },
-    {
-      "stevearc/dressing.nvim",
-      lazy = true,
-    },
-    {
-      "kdheepak/cmp-latex-symbols",
-      dependencies = "hrsh7th/nvim-cmp",
-    },
-    {
-      "ThePrimeagen/refactoring.nvim",
-      lazy = true,
-    },
-    {
-      "b0o/incline.nvim",
-    },
-    {
-      "fgheng/winbar.nvim",
-    },
-    {
-      "SmiteshP/nvim-gps",
-      dependencies = "nvim-treesitter/nvim-treesitter",
-    },
-    {
-      "vimpostor/vim-tpipeline",
-    },
-    {
-      "stevearc/overseer.nvim",
-    },
-    {
-      "nvim-neo-tree/neo-tree.nvim",
-      dependencies = {
-        "MunifTanjim/nui.nvim",
-      },
-    },
+    { "karb94/neoscroll.nvim" },
+    { "declancm/cinnamon.nvim" },
+    { "github/copilot.vim" },
+    { "zbirenbaum/copilot.lua",                      dependencies = { "zbirenbaum/copilot-cmp", "nvim-cmp" } },
+    { "ThePrimeagen/harpoon",                        dependencies = { { "nvim-lua/plenary.nvim" },
+      { "nvim-lua/popup.nvim" } } },
+    { "sindrets/diffview.nvim" },
+    { "chipsenkbeil/distant.nvim" },
+    { "abzcoding/nvim-mini-file-icons" },
+    { "mtdl9/vim-log-highlighting",                  ft = { "text", "log" } },
+    { "yamatsum/nvim-cursorline" },
+    { "abecodes/tabout.nvim" },
+    { "kevinhwang91/nvim-hlslens" },
+    { "chrisbra/csv.vim" },
+    { "nvim-treesitter/nvim-treesitter-textobjects", lazy = true,                                                             dependencies =
+    "nvim-treesitter" },
+    { "sidebar-nvim/sidebar.nvim" },
+    { "skywind3000/asynctasks.vim",                  dependencies = { { "skywind3000/asyncrun.vim" } } },
+    { "scalameta/nvim-metals",                       dependencies = { "nvim-lua/plenary.nvim" } },
+    { "jbyuki/instant.nvim" },
+    { "nvim-telescope/telescope-file-browser.nvim" },
+    { "j-hui/fidget.nvim" },
+    { "michaelb/sniprun" },
+    { "liuchengxu/vista.vim" },
+    { "p00f/clangd_extensions.nvim" },
+    { "saecki/crates.nvim",                          dependencies = { { "nvim-lua/plenary.nvim" } } },
+    { "hrsh7th/cmp-cmdline" },
+    { "mrjones2014/legendary.nvim" },
+    { "stevearc/dressing.nvim",                      lazy = true },
+    { "kdheepak/cmp-latex-symbols",                  dependencies = "hrsh7th/nvim-cmp" },
+    { "ThePrimeagen/refactoring.nvim",               lazy = true },
+    { "b0o/incline.nvim" },
+    { "fgheng/winbar.nvim" },
+    { "SmiteshP/nvim-gps",                           dependencies = "nvim-treesitter/nvim-treesitter" },
+    { "vimpostor/vim-tpipeline" },
+    { "stevearc/overseer.nvim" },
+    { "nvim-neo-tree/neo-tree.nvim",                 dependencies = { "MunifTanjim/nui.nvim" } },
     { "MunifTanjim/nui.nvim" },
-    {
-      "folke/noice.nvim",
-      dependencies = {
-        "rcarriga/nvim-notify",
-      }
-    },
-    {
-      "olexsmir/gopher.nvim",
-    },
-    {
-      "leoluz/nvim-dap-go",
-    },
-    {
-      "AckslD/swenv.nvim",
-    },
-    {
-      "mfussenegger/nvim-dap-python",
-    },
-    {
-      "mxsdev/nvim-dap-vscode-js",
-      lazy = true,
-    },
-    {
-      "smjonas/inc-rename.nvim",
-      lazy = true,
-    },
-    {
-      "m-demare/hlargs.nvim",
-      lazy = true,
-      dependencies = { "nvim-treesitter/nvim-treesitter" },
-    },
-    {
-      "cshuaimin/ssr.nvim",
-      lazy = true,
-    },
-    {
-      "Civitasv/cmake-tools.nvim",
-    },
-    {
-      "raimon49/requirements.txt.vim",
-    },
-    {
-      "phaazon/mind.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
-    },
+    { "folke/noice.nvim",                            dependencies = { "rcarriga/nvim-notify" } },
+    { "olexsmir/gopher.nvim" },
+    { "leoluz/nvim-dap-go" },
+    { "AckslD/swenv.nvim" },
+    { "mfussenegger/nvim-dap-python" },
+    { "mxsdev/nvim-dap-vscode-js",                   lazy = true },
+    { "smjonas/inc-rename.nvim",                     lazy = true },
+    { "m-demare/hlargs.nvim",                        lazy = true,                                                             dependencies = {
+      "nvim-treesitter/nvim-treesitter" } },
+    { "cshuaimin/ssr.nvim",                          lazy = true, },
+    { "Civitasv/cmake-tools.nvim" },
+    { "raimon49/requirements.txt.vim" },
+    { "phaazon/mind.nvim",                           dependencies = { "nvim-lua/plenary.nvim" }, },
     { "ibhagwan/fzf-lua" },
     { "folke/flash.nvim" },
-    { "piersolenski/wtf.nvim",              dependencies = { "MunifTanjim/nui.nvim", } },
+    { "piersolenski/wtf.nvim",                       dependencies = { "MunifTanjim/nui.nvim" } },
     { "james1236/backseat.nvim" },
     { "lukas-reineke/indent-blankline.nvim" }
-
-
   }
 end
 
