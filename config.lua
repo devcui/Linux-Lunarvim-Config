47587c5ae862d33d5e0f26259845b79bab47c248
devcui <devcui@outlook.com>
2023-11-12 10:49:27 +0800

    fix(lsp): fix clangd lsp

--- config.lua
+++ config.lua
@@ -149,3 +149,13 @@ require("user.autocommands").config()
 -- Additional Keybindings
 -- =========================================
 require("user.keybindings").config()
+
+-- clangd fix
+local cmp_nvim_lsp = require "cmp_nvim_lsp"
+require("lspconfig").clangd.setup {
+  capabilities = cmp_nvim_lsp.default_capabilities(),
+  cmd = {
+    "clangd",
+    "--offset-encoding=utf-16",
+  },
+}
